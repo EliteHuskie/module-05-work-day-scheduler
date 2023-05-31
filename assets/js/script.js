d3706d38f7a92a0d96ab921e81c435868ab1533c
Jared Stratton <Stratton_10@hotmail.com>
2023-05-31 11:20:56 -0300

    Added code block for updating color coding, checking blockhour and then if its past, current or future hour

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,23 @@
-/* Function which contains variable for checking currentHour using dayjs and format below */
+// Function which contains variable for checking currentHour using dayjs and format below
 $(function () {
   var currentHour = dayjs().format("H");
+
+// Function to update the color coding of time blocks
+function updateTimeBlockColors() {
+  $(".time-block").each(function () {
+    var blockHour = parseInt($(this).attr("id").split("-")[1]);
+
+    // Remove any existing classes before applying the new one
+    $(this).removeClass("past present future");
+    // Checks blockhour against current hour and then adds class for past, present or future
+    if (blockHour < currentHour) {
+      $(this).addClass("past");
+    } else if (blockHour == currentHour) {
+      $(this).addClass("present");
+    } else {
+      $(this).addClass("future");
+    }
+  });
+}
 
 });
